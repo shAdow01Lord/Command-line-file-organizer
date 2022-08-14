2b7087fba3438322d392ded99ab2e62ff24ad3c9
Rajeev <singhrajeevranjan404@gmail.com>
2022-08-14 06:35:54 +0530

    Initial commit

---
+++ utility.js
@@ -0,0 +1,28 @@
+let utility ={}
+utility.types = {
+    mediaVideo: ["mp4", "mkv"],
+    mediaAudio: ["mp3", "wma", "MP3"],
+    mediaImages: ["png", "jpg", "gif", "jpeg", "jfif"],
+    archives: ["zip", "7z", "rar", "tar", "gz", "ar", "iso", "xz"],
+    documents: [
+        "docx",
+        "doc",
+        "pdf",
+        "txt",
+        "xlsx",
+        "pptx",
+        "xls",
+        "odt",
+        "ods",
+        "odp",
+        "odg",
+        "odf",
+        "ps",
+        "ppt",
+        "tex",
+    ],
+    code: ["cpp", "py", "java", "html", "php", "cgi"],
+    app: ["exe", "dmg", "pkb", "deb", "apk", "apks"],
+    webpages: ["webp"],
+}
+module.exports = utility;
